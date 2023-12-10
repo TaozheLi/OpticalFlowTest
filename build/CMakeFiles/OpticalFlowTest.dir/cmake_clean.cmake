78d1b56157d4abda277aad0401d5bc611add15d6
TaozheLi <Taozhe.Li-1@ou.edu>
2023-12-09 20:35:25 -0600

    with build file in ubuntu

---
+++ build/CMakeFiles/OpticalFlowTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpticalFlowTest.dir/main.cpp.o"
+  "OpticalFlowTest"
+  "OpticalFlowTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpticalFlowTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
